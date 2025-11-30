3aefb072f0120769a7b9935532b0a1013aeb3504
rendive <rendhivecode@gmail.com>
2025-11-30 13:05:56 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul gzip.py

---
+++ 1. Mengimpor Modul gzip.py
@@ -0,0 +1,5 @@
+import gzip
+
+print("Modul gzip berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk melakukan kompresi dan dekompresi dengan format GZIP.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari gzip.
