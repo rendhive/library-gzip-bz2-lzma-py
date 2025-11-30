edb7cbc4e5472e48cdbd8cd416d7f9b1c8ba599f
rendive <rendhivecode@gmail.com>
2025-11-30 13:09:18 +0700

    [DOCT] Sample Library Python: 3. Mengimpor Modul lzma.py

---
+++ 3. Mengimpor Modul lzma.py
@@ -0,0 +1,5 @@
+import lzma
+
+print("Modul lzma berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk melakukan kompresi dan dekompresi dengan format LZMA.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari lzma.
