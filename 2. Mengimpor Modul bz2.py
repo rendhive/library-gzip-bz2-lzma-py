6d8b9a6c7b5a5fadebc5692756e9c58bf2b60692
rendive <rendhivecode@gmail.com>
2025-11-30 13:07:50 +0700

    [DOCT] Sample Library Python: 2. Mengimpor Modul bz2.py

---
+++ 2. Mengimpor Modul bz2.py
@@ -0,0 +1,5 @@
+import bz2
+
+print("Modul bz2 berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk melakukan kompresi dan dekompresi dengan format BZ2.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari bz2.
